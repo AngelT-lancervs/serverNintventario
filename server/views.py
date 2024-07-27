12f58fa6046f5ecb4f3a7fd850da01cbc73605d8
lancervs <aatomala@espol.edu.ec>
2024-07-26 21:40:10 -0500

    update host

--- server/views.py
+++ server/views.py
@@ -20,7 +20,7 @@ from rest_framework.authentication import TokenAuthentication
 
 UPLOAD_DIR = 'uploads'
 PYTHON_SCRIPT_PATH = '/scripts/excel_wirtter.py'
-UPLOAD_URL = 'http://192.168.1.3:8000/upload-excel/'
+UPLOAD_URL = 'https://servernintventario.onrender.com/upload-excel/'
 
 @api_view(['POST'])
 def login(request):
